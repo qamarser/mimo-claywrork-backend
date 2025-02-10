2d2fcfdfa6504c86ea21db3eb61636e56c64d61e
Acil <acilhamieh4@gmail.com>
2025-02-10 15:09:24 +0200

    peoductcontroller code

---
+++ controllers/productController.js
@@ -0,0 +1,24 @@
+const Product = require('../models/Product');
+
+// Fetch all products
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Add a new product
+const createProduct = async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    const savedProduct = await product.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = { getProducts, createProduct };
