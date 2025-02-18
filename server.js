c2b9938ea41a62b201a2db86c586e95e275aff3f
qamar serhan <serhanqamar@gmail.com>
2025-02-18 22:57:18 +0200

    test

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json()) //to make sure he data from front to back is in the form
 app.use(cors({
   origin: 'https://mimo-claywrork-frontend.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  credentials: true
 }));
 
 
